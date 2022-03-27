4fb5ab94e499feed3309a9b448ac4726775426c3
icaseres <jhon091199@gmail.com>
2022-03-26 20:32:29 -0500

    add property

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { config } = require('./config');
+const { config } = require("./config");
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
@@ -7,16 +7,17 @@ const DB_NAME = config.dbName;
 
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`;
 const conectarDB = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("DB is connected");
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
+        console.log("DB is connected");
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 };
 
 module.exports = conectarDB;
